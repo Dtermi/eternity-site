25407e2d18466c30f97b3688fa76df648b95b82e
Dtermi <kirillbulass5@gmail.com>
2026-02-19 14:10:40 +0100

    Create main-animations.js

---
+++ main-animations.js
@@ -0,0 +1,9 @@
+function revealOnScroll() {
+  document.querySelectorAll(".reveal").forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+}
+window.addEventListener("scroll", revealOnScroll);
